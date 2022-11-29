9f6ffe73b97240d7bb1196e8c49740d4e49ebb44
jorge17alfe <jorgeordonezmorales@gmail.com>
2022-11-29 11:59:39 +0100

    29/11/2022 2

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,14 +1,14 @@
 module.exports ={
-    database:{
-        host:'localhost',
-        user:'root',
-        password :'',
-        database : 'db_micartaonline' 
-    },
     // database:{
-    //     host:'db5011009529.hosting-data.io',
-    //     user:'dbu2392463',
-    //     password :'Micartaonline1+',
-    //     database : 'dbs9305907' 
-    // }
+    //     host:'localhost',
+    //     user:'root',
+    //     password :'',
+    //     database : 'db_micartaonline' 
+    // },
+    database:{
+        host:'db5011009529.hosting-data.io',
+        user:'dbu2392463',
+        password :'Micartaonline1+',
+        database : 'dbs9305907' 
+    }
 }
